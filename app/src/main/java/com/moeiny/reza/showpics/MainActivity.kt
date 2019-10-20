67c69912633a107edce3c7131ae3fdf199c6f2ca
reza575 <h_moeiny@yahoo.com>
2019-10-21 02:30:32 +1100

    creating models

---
+++ app/src/main/java/com/moeiny/reza/showpics/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.moeiny.reza.showpics
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.moeiny.reza.guesspic.presenter.PhotoService
+import com.moeiny.reza.showpics.viewmodel.AppViewModel
+import com.moeiny.reza.showpics.adapter.UserAdapter
+import com.moeiny.reza.showpics.model.entity.users.Users
+import com.moeiny.reza.showpics.utils.OptusCallback
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    lateinit var usersList: List<Users>
+    lateinit var recyclerView: RecyclerView
+    lateinit var userAdapter: UserAdapter
+    lateinit var context : Context
+    lateinit var appViewModel: AppViewModel
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        context=this
+        appViewModel= ViewModelProviders.of(this).get(AppViewModel::class.java);
+
+//        appViewModel.selectedAlbumId.observe(this, object : Observer<String>() {
+//            fun onChanged(@Nullable integer: Int?) {
+//                Log.i(TAG, "onChanged: ")
+//            }
+//        })
+        setUpView()
+        getUsersInfo()
+
+    }
+
+
+    fun getUsersInfo() {
+
+        PhotoService.getUsersInfo(object : OptusCallback<List<Users>, Throwable> {
+
+            override fun onSuccess(result: List<Users>) {
+                usersList = ArrayList<Users>()
+                appViewModel.setUserList(result)
+                setDataOnRecycler()
+            }
+
+            override fun onError(error: Throwable?) {
+                Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onComplete() {
+                print("complete")
+            }
+
+        })
+    }
+
+    fun setDataOnRecycler() {
+        recyclerView.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = UserAdapter(context!!, appViewModel.getUserList())
+    }
+
+    fun setUpView() {
+        recyclerView = findViewById(R.id.rv_fragmentUser_users)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+    }
+}
+
---
+++ app/src/main/java/com/moeiny/reza/showpics/model/entity/users/Company.kt
@@ -0,0 +1,7 @@
+package com.moeiny.reza.showpics.model.entity.users
+
+data class Company(
+    val bs: String,
+    val catchPhrase: String,
+    val name: String
+)
