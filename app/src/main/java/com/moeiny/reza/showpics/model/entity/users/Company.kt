aba3480945303c7a37a6a5ae07ddac47c7cf3c90
reza575 <h_moeiny@yahoo.com>
2019-10-21 03:34:33 +1100

    update

--- app/src/main/java/com/moeiny/reza/showpics/model/entity/users/Company.kt
+++
@@ -1,7 +0,0 @@
-package com.moeiny.reza.showpics.model.entity.users
-
-data class Company(
-    val bs: String,
-    val catchPhrase: String,
-    val name: String
-)
--- app/src/main/java/com/moeiny/reza/showpics/model/entity/users/Geo.kt
+++
@@ -1,6 +0,0 @@
-package com.moeiny.reza.showpics.model.entity.users
-
-data class Geo(
-    val lat: String,
-    val lng: String
-)
