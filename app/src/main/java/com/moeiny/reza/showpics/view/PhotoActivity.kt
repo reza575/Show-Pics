9127a3db41a7a066cf3f720ceb40db82d0f17f79
reza575 <h_moeiny@yahoo.com>
2019-10-21 02:42:01 +1100

    create views

---
+++ app/src/main/java/com/moeiny/reza/showpics/view/PhotoActivity.kt
@@ -0,0 +1,82 @@
+package com.moeiny.reza.showpics.view
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.moeiny.reza.guesspic.presenter.PhotoService
+import com.moeiny.reza.showpics.R
+import com.moeiny.reza.showpics.viewmodel.AppViewModel
+import com.moeiny.reza.showpics.adapter.PhotosAdapter
+import com.moeiny.reza.showpics.model.entity.Photo
+import com.moeiny.reza.showpics.utils.OptusCallback
+
+class PhotoActivity : AppCompatActivity() {
+    lateinit var photoList: List<Photo>
+    lateinit var recyclerView: RecyclerView
+    lateinit var photosAdapter: PhotosAdapter
+    lateinit var txtTitle: TextView
+    lateinit var id:String
+    lateinit var context : Context
+    lateinit var appViewModel: AppViewModel
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo)
+        context=this
+        appViewModel= ViewModelProviders.of(this).get(AppViewModel::class.java);
+
+        val bundle = intent.extras
+        appViewModel.setAlbumId (bundle?.getString("albumId").toString())
+
+        getPhotoInfo()
+        setUpView()
+    }
+
+    fun getPhotoInfo() {
+
+        PhotoService.getPhotosInfo(object : OptusCallback<List<Photo>, Throwable> {
+
+            override fun onSuccess(result: List<Photo>) {
+                photoList = ArrayList<Photo>()
+                photoList = result
+                setDataOnRecycler()
+            }
+
+            override fun onError(error: Throwable?) {
+                Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onComplete() {
+                print("complete")
+            }
+
+        })
+    }
+
+    fun setDataOnRecycler() {
+        val photoListTemp:ArrayList<Photo> = ArrayList<Photo>()
+        for(item:Photo in photoList ){
+            if(item.albumId.toString().equals( appViewModel.getAlbumId())){
+                photoListTemp.add(item)
+            }
+            appViewModel.setPhotoList(photoListTemp)
+        }
+
+        recyclerView.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = PhotosAdapter(context!!, appViewModel.getPhotoList())
+    }
+
+    fun setUpView() {
+        txtTitle = findViewById(R.id.txt_fragmentphoto_title)
+        recyclerView = findViewById(R.id.rv_fragmentphoto_photo)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        txtTitle.setText("Album Id : "+appViewModel.getAlbumId())
+    }
+}
---
+++ app/src/main/java/com/moeiny/reza/showpics/view/ShowActivity.kt
@@ -0,0 +1,44 @@
+package com.moeiny.reza.showpics.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProviders
+import com.moeiny.reza.showpics.R
+import com.moeiny.reza.showpics.viewmodel.AppViewModel
+import com.squareup.picasso.Picasso
+
+class ShowActivity : AppCompatActivity() {
+
+    lateinit var txtalbumid: TextView
+    lateinit var txtphotoid: TextView
+    lateinit var txttitle: TextView
+    lateinit var imgPicUrl: ImageView
+    lateinit var appViewModel: AppViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show)
+        appViewModel= ViewModelProviders.of(this).get(AppViewModel::class.java);
+
+        val bundle = intent.extras
+        appViewModel.setAlbumId(bundle?.getString("albumId").toString())
+        appViewModel.setPhotoId(bundle?.getString("photoid").toString())
+        appViewModel.seturl(bundle?.getString("url").toString())
+        appViewModel.setTitle(bundle?.getString("title").toString())
+
+        setUpView()
+    }
+
+    fun setUpView() {
+        txtalbumid = findViewById(R.id.txt_showFragment_albumid)
+        txtphotoid = findViewById(R.id.txt_showFragment_photoid)
+        txttitle = findViewById(R.id.txt_showFragment_title)
+        imgPicUrl = findViewById(R.id.img_showFragment_photo)
+        txtalbumid.setText("Album Id : "+appViewModel.getAlbumId())
+        txtphotoid.setText("Photo Id : "+appViewModel.getPhotoId())
+        txttitle.setText(appViewModel.getTitle())
+        Picasso.get().load(appViewModel.geturl()).into(imgPicUrl)
+    }
+}
